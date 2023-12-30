09bfe9b3484fbdde5bed771d5a6be89307f06bb6
Takumi19980801 <takunomi1998@gmail.com>
2023-12-30 14:54:52 +0900

    third commit

--- db/migrate/20231229031016_create_comments.rb
+++ db/migrate/20231229031016_create_comments.rb
@@ -1,8 +1,6 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :user_id,      null: false
-      t.integer :prototype_id, null: false
       t.text :content,         null: false
       t.references :prototype, null: false, foreign_key: { to_table: :prototypes }
       t.references :user, null: false, foreign_key: { to_table: :users }
